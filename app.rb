e18af4b32116d0735a1de604340efc66e46117ce
jtraxler89 <jeff.traxler@kellogg.northwestern.edu>
2020-06-09 00:40:32 +0000

    Initial commit

--- app.rb
+++ app.rb
@@ -15,5 +15,53 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+bars_table = DB.from(:bars)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+get "/" do 
+    @bars = bars_table.all
+    puts @bars.inspect
+    view "bars"
+end
+
+get "/bars/:id" do
+    # SELECT * FROM bars WHERE id=:id
+    @bar = bars_table.where(:id => params["id"]).to_a[0]
+    @reviews = reviews_table.where(:bars_id => params["id"]).to_a
+    @reviewsCTdayofweekM = reviews_table.where(:bars_id => params["id"], :dayofweek => "Monday").count
+    @reviewsCTdayofweekTu = reviews_table.where(:bars_id => params["id"], :dayofweek => "Tuesday").count
+    @reviewsCTdayofweekW = reviews_table.where(:bars_id => params["id"], :dayofweek => "Wednesday").count
+    @reviewsCTdayofweekTh = reviews_table.where(:bars_id => params["id"], :dayofweek => "Thursday").count
+    @reviewsCTdayofweekF = reviews_table.where(:bars_id => params["id"], :dayofweek => "Friday").count
+    @reviewsCTdayofweekSa = reviews_table.where(:bars_id => params["id"], :dayofweek => "Saturday").count
+    @reviewsCTdayofweekSu = reviews_table.where(:bars_id => params["id"], :dayofweek => "Sunday").count
+    @reviewsAVGdrinkspecials = reviews_table.where(:bars_id => params["id"]).avg(:drinkspecials)
+    @reviewsAVGfoodspecials = reviews_table.where(:bars_id => params["id"]).avg(:foodspecials)
+    @reviewsAVGambiance = reviews_table.where(:bars_id => params["id"]).avg(:ambiance)
+    @reviewsAVGoverall = reviews_table.where(:bars_id => params["id"]).avg(:overall)
+    puts @bar.inspect
+    puts params.inspect
+    view "bar"
+end
+
+get "/bars/:id/reviews/new" do
+    @bar = bars_table.where(:id => params["id"]).to_a[0]
+    view "new_review"
+end
+
+get "/bars/:id/reviews/create" do
+    puts params.inspect
+    reviews_table.insert(:bars_id => params["id"],
+                         :dayofweek => params["dayofweek"],
+                         :drinkspecials => params["drinkspecials"],
+                         :foodspecials => params["foddspecials"],
+                         :ambiance => params["ambiance"],
+                         :overall => params["overall"],
+                         :firstname => params["firstname"],
+                         :lastinitial => params["lastinitial"],
+                         :email => params["email"],
+                         :reviewtitle => params["reviewtitle"],
+                         :comments => params["comments"])
+    "Thank you for your review!"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,100 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :bars do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
-end
-DB.create_table! :rsvps do
-  primary_key :id
-  foreign_key :event_id
-  Boolean :going
   String :name
+  String :address, text: true
+  String :telephone
+  String :website
+  String :neighborhood
+end
+DB.create_table! :reviews do
+  primary_key :id
+  foreign_key :bars_id
+  foreign_key2 :users_id
+  String :dayofweek
+  Integer :drinkspecials
+  Integer :foodspecials
+  Integer :ambiance
+  Integer :overall
+  String :firstname
+  String :lastinitial
   String :email
+  String :reviewtitle
   String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :firstname
+  String :lastinitial
+  String :homezipcode
+  String :email
+  String :password
+  Boolean :weeklyupdate
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+bars_table = DB.from(:bars)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+bars_table.insert(name: "Five & Dime",
+                  address: "1026 Davis St, Evanston, IL 60201",
+                  telephone: "(847) 869-4343",
+                  website: "https://www.fiveanddimeevanston.com/",
+                  neighborhood: "Evanston")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+bars_table.insert(name: "Bangers & Lace",
+                  address: "810 Grove St, Evanston, IL 60201",
+                  telephone: "(847) 905-0854",
+                  website: "http://www.bangersandlaceevanston.com/",
+                  neighborhood: "Evanston")
+
+bars_table.insert(name: "Whiskey Thief Tavern",
+                  address: "616 DAvis St, Evanston, IL 60201",
+                  telephone: "(847) 859-2342",
+                  website: "https://www.whiskeythieftavern.com/",
+                  neighborhood: "Evanston")                  
+
+bars_table.insert(name: "Bat 17",
+                  address: "1709 Benson Ave, Evanston, IL 60201",
+                  telephone: "(847) 733-7117",
+                  website: "http://bat17evanston.com/",
+                  neighborhood: "Evanston")                  
+
+bars_table.insert(name: "Evanston Pub",
+                  address: "1601 Sherman Ave, Evanston, IL 60201",
+                  telephone: "(847) 328-3688",
+                  website: "https://www.facebook.com/evanstonpub/",
+                  neighborhood: "Evanston")                  
+
+bars_table.insert(name: "Celtic Knot Public House",
+                  address: "626 Chirch St, Evanston, IL 60201",
+                  telephone: "(847) 864-1679",
+                  website: "https://www.celticknotpub.com/",
+                  neighborhood: "Evanston")                  
+
+reviews_table = DB.from(:reviews)
+
+reviews_table.insert(bars_id: 1,
+                     dayofweek: "Friday",
+                     drinkspecials: 5,
+                     foodspecials: 5,
+                     ambiance: 5,
+                     overall: 5,
+                     firstname: "Jeff",
+                     lastinitial: "T",
+                     email: "jtraxler89@gmail.com",
+                     reviewtitle: "CRUSHED IT",
+                     comments: "That rooftop is sick. And they really had their social distancing down.")
+
+reviews_table.insert(bars_id: 1,
+                     dayofweek: "Tuesday",
+                     drinkspecials: 2,
+                     foodspecials: 2,
+                     ambiance: 2,
+                     overall: 2,
+                     firstname: "John",
+                     lastinitial: "T",
+                     email: "jtraxler@wi.rr.com",
+                     reviewtitle: "YUCK!!",
+                     comments: "There were so many MBA students going crazy...")
